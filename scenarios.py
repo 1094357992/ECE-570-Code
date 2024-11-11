5b7d4e2b8086af69101226fe31679f7a114dead7
1094357992 <150397792+1094357992@users.noreply.github.com>
2024-11-10 21:29:58 -0500

    Create scenarios.py

---
+++ scenarios.py
@@ -0,0 +1,18 @@
+class ScenarioManager:
+    def __init__(self):
+        self.scenarios = {}
+    
+    def add_scenario(self, name: str, parameters: dict):
+        """Add a new climate scenario."""
+        self.scenarios[name] = parameters
+    
+    def compare_scenarios(self, scenario1: str, scenario2: str, variable: str):
+        """Compare two climate scenarios."""
+        model1 = ClimSim(**self.scenarios[scenario1])
+        model2 = ClimSim(**self.scenarios[scenario2])
+        
+        data1 = model1.get_variable_data(variable)
+        data2 = model2.get_variable_data(variable)
+        
+        difference = data2 - data1
+        return difference
