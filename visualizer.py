fd06fb69f827109a8f0e506e6aa4efeec43f9c33
1094357992 <150397792+1094357992@users.noreply.github.com>
2024-11-10 21:28:28 -0500

    Create visualizer.py

---
+++ visualizer.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from climsim import ClimSim
+
+class ClimateVisualizer:
+    def __init__(self, model: ClimSim):
+        self.model = model
+        self.data = None
+        
+    def plot_temperature_distribution(self, year: int):
+        """Plot global temperature distribution for a specific year."""
+        temp_data = self.model.get_temperature(year)
+        plt.figure(figsize=(12, 6))
+        sns.heatmap(temp_data, cmap='RdBu_r', center=0)
+        plt.title(f'Global Temperature Distribution - Year {year}')
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        return plt.gcf()
+    
+    def plot_time_series(self, variable: str, region: tuple = None):
+        """Plot time series of climate variables."""
+        data = self.model.get_variable_data(variable)
+        if region:
+            lat_min, lat_max, lon_min, lon_max = region
+            data = data.sel(lat=slice(lat_min, lat_max), lon=slice(lon_min, lon_max))
+        
+        plt.figure(figsize=(10, 5))
+        data.mean(dim=['lat', 'lon']).plot()
+        plt.title(f'{variable} Time Series')
+        return plt.gcf()
