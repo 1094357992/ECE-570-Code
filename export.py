27cd6a1d5f500734e77335f45146e8250a4fef55
1094357992 <150397792+1094357992@users.noreply.github.com>
2024-11-10 21:31:18 -0500

    Update export.py

--- export.py
+++ export.py
@@ -4,18 +4,15 @@ import xarray as xr
 class DataExporter:
     @staticmethod
     def to_csv(data: xr.DataArray, filename: str):
-        """Export climate data to CSV format."""
         df = data.to_dataframe()
         df.to_csv(filename)
     
     @staticmethod
     def to_netcdf(data: xr.DataArray, filename: str):
-        """Export climate data to NetCDF format."""
         data.to_netcdf(filename)
     
     @staticmethod
     def create_report(analyzer: ClimateAnalyzer, scenarios: list, variables: list):
-        """Create a summary report of climate analysis."""
         report = pd.DataFrame()
         for scenario in scenarios:
             for variable in variables:
